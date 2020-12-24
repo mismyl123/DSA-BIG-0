f33c293bcf255c378b65dca02c170a3eb1979d4b
mismyl123 <mlalonde309@gmail.com>
2020-12-23 19:41:06 -0500

    Create BigO.js

---
+++ BigO.js
@@ -0,0 +1,52 @@
+//1. What is the Big O for this?
+//  1. O(1) - Constant
+//  2. O(n) - Linear
+
+//2. Even or Odd
+//Logarithmic time O(log(n)) - becauseut cuts the problem in half
+
+//3. Are you here?
+//Polynomial O(n^k) - nested loop
+
+//4. Doubler
+//Linear O(n) - because you do one operation for each value in the array
+
+//5. Naive search
+//Linear O(n) - goes through each value of the array
+// best case scenario - O(1) - Constant where the 1st value is the item.
+
+//6. Creating pairs
+//Polynimial O(n^k) - nested loop
+
+//7. Compute the sequence
+//Fibonnici sequence - Linear O(n)
+
+
+1. O(1);
+2. O(n);
+
+
+
+Even or Odd
+O(1)
+
+
+Are You Here?
+O(2^n)
+
+Doubler
+O(n)
+
+
+Naive Search
+O(n)
+
+
+Creating Pairs
+O(n^k)
+
+Compute the sequence
+O(n)
+
+An efficient Search
+O(log(n)
